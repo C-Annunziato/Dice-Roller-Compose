bac6efbb2b041ba3cd2d3c6006775c46f35254ab
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2023-01-09 15:48:52 -0500

    cleaned up ui

--- app/src/main/java/com/example/dicerollercompose/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercompose/MainActivity.kt
@@ -5,18 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dicerollercompose.ui.theme.DiceRollerComposeTheme
 import kotlin.random.Random
 
@@ -25,13 +25,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
                     DiceRollerApp()
-                }
             }
         }
     }
@@ -66,8 +60,11 @@ fun DiceWithButtonAndImage(
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             result = Random.nextInt(1, 7)
-        }) {
-            Text(stringResource(id = R.string.roll))
+        }, modifier = Modifier.size(89.dp),
+            shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(Color.LightGray)
+        ) {
+            Text(stringResource(id = R.string.roll), fontSize = 20.sp)
         }
     }
 }
